6159e6f5597c731ef7196f2f719df1cabecee857
melik.kutluay <melik.kutluay@softtech.com.tr>
2019-11-03 12:36:12 +0300

    second

---
+++ src/components/User.vue
@@ -0,0 +1,42 @@
+<template>
+    <div class="component">
+        <h1>The User Component</h1>
+        <p>I'm an awesome User!</p>
+        <hr>
+        <div class="row">
+            <div class="col-xs-12 col-sm-6">
+                <app-user-detail
+                :myName="name"
+                @nameWasReset="name = $event"
+                :userAge="age"></app-user-detail>
+            </div>
+            <div class="col-xs-12 col-sm-6">
+                <app-user-edit :userAge="age"></app-user-edit>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    import UserDetail from './UserDetail.vue';
+    import UserEdit from './UserEdit.vue';
+
+    export default {
+        data:function(){
+          return{
+              name : 'Melik',
+              age : 27
+          }
+        },
+        components: {
+            appUserDetail: UserDetail,
+            appUserEdit: UserEdit
+        }
+    }
+</script>
+
+<style scoped>
+    div {
+        background-color: lightblue;
+    }
+</style>
---
+++ src/components/UserDetail.vue
@@ -0,0 +1,37 @@
+<template>
+    <div class="component">
+        <h3>You may view the User Details here</h3>
+        <p>Many Details</p>
+        <p>UserName:{{ switchName() }}</p>
+        <button @click="resetName()">Reset Name</button>
+        <button @click="reserFn()">Reset Name </button>
+        <button></button>
+    </div>
+</template>
+
+<script>
+    export default {
+        data: function () {
+            return{
+
+            }
+        },
+        methods:{
+            switchName(){
+
+            },
+            resetName(){
+
+            },
+            resetFn(){
+
+            }
+        }
+    }
+</script>
+
+<style scoped>
+    div {
+        background-color: lightcoral;
+    }
+</style>
